43533e4ac1ac605074c0c1fb6074cb201eb4b208
Aryan apte <74185236+Aryan190902@users.noreply.github.com>
2022-08-17 16:45:03 +0530

    key exposure
    secret key removed

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s#@n%tvw-&#c)$n^l!oi16$upf_*_s4bu5+^926otta%*vph^*'
+SECRET_KEY = BLOGGEN_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
